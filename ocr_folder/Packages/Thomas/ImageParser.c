92641b0f06caf18a27c7b83c41cce967b88256a1
Thomas Saison <thomas.saison@epita.fr>
2018-11-29 18:28:30 +0100

    ok

--- ocr_folder/Packages/Thomas/ImageParser.c
+++ ocr_folder/Packages/Thomas/ImageParser.c
@@ -3,44 +3,77 @@
 #include "TraitementImage.h"
 #include "ImageParser.h"
 
-
-size_t Line(unsigned char* line,size_t i,size_t j,BMPPic_ RLSAPic,BMPPic_ myPic){
-	int index = 0;
-	while(myPic.GREYMATRIX[i][j] == 0){
-		printf("j = %d \n", j);
-		line = realloc(line,index + 1);
-		line[index] = myPic.GREYMATRIX[i][j];
-		j += 1;
-	}
-	return j;
-}
-
-unsigned char ** InitMatrix(unsigned char** matrix,size_t height,size_t width,unsigned char value){
-	matrix = malloc(height * width * sizeof(unsigned char));
+void SaveMatrixToFile(unsigned char ** ARRAY, size_t height,size_t width){
+	unsigned char * MATRIX = malloc(height * width * sizeof(unsigned char));
 	for (size_t i = 0; i < height; i++) {
-		matrix[i] = malloc(width * sizeof(unsigned char));
 		for (size_t j = 0; j < width; j++) {
-			matrix[i][j] = value;
+			MATRIX[i*width + j] = ARRAY[i][j];
 		}
 	}
-	return matrix;
+	FILE* file = fopen("DATABASE","w+");
+	fwrite(MATRIX,1,height*width,file);
+	fclose(file);
+	free(MATRIX);
+}
+
+unsigned char ** InitMatrix(size_t height, size_t width, unsigned char value){
+	unsigned char  ** ARRAY = malloc(height * width * sizeof(unsigned char));
+	for (size_t i = 0; i < height; i++) {
+		ARRAY[i] = malloc(width * sizeof(unsigned char));
+		for (size_t j = 0; j < width; j++) {
+			ARRAY[i][j] = value;
+		}
+	}
+	return ARRAY;
 }
 
 BMPPic_ getTextZones(BMPPic_ myPic, BMPPic_ RLSAPic){
-	unsigned char **alreadyTreat;
-	alreadyTreat = InitMatrix(**alreadyTreat,myPic.height,myPic.width,0);
-	printf("%d \n", alreadyTreat[0][0]);
+
+	unsigned char ***ZONES;
+	ZONES = malloc(1);*
+	size_t size_of_ZONES = 1;
+
+	unsigned char ** alreadyTreat = InitMatrix(myPic.height,myPic.width,0);
+
 	for (size_t i = 0; i < myPic.height; ++i)
 	{
 		for (size_t j = 0; j < myPic.width; ++j)
 		{
 			if(RLSAPic.GREYMATRIX[i][j] == 0 && alreadyTreat[i][j] == 0)
 			{
-				alreadyTreat[i][j] = 1;
+				unsigned char **TAB = malloc(1);
+				size_t tp_i = i;
+				size_t TABLength = 1;
+				size_t height = 0;
+				size_t width = 0;
+				while (RLSAPic.GREYMATRIX[tp_i][j] == 0)
+				{
+					size_t tp_j = j;
+					unsigned char * LINE = malloc(1);
+					while (RLSAPic.GREYMATRIX[tp_i][tp_j] == 0)
+					{
+						LINE = (unsigned char*) realloc(LINE,(tp_j-j+1));
+						LINE[tp_j - j] = myPic.GREYMATRIX[tp_i][tp_j];
+						alreadyTreat[tp_i][tp_j] = 1;
+						if (width < tp_j - j) {
+							width = tp_j - j;
+						}
+						++ tp_j;
+					}
+					TABLength += tp_j - j;
+					TAB = (unsigned char **) realloc(TAB,TABLength);
+					TAB[tp_i - i] = malloc(tp_j - j);
+					TAB[tp_i - i] = LINE;
+					if (height < tp_i - i) {
+						height = tp_i - i;
+					}
+					++ tp_i;
+				}
+				SaveMatrixToFile(TAB,height,width);
 			}
 		}
 	}
-
+	//SaveMatrixToFile(myPic.GREYMATRIX,myPic.height,myPic.width);
 	for (size_t i = 0; i < myPic.height; ++i)
 	{
 		free(alreadyTreat[i]);
